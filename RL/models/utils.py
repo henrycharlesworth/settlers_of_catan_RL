e44bfad444c7a5b0ed50c5f6264c4a3ae7ce7021
henrycharlesworth <henrycharlesworth@rowdentech.com>
2021-12-08 12:22:02 +0000

    fix vf norm for cuda training

--- RL/models/utils.py
+++ RL/models/utils.py
@@ -11,9 +11,11 @@ class ValueFunctionNormaliser(nn.Module):
         self.dummy_param = nn.Parameter(torch.empty(0))
         self.mean = nn.Parameter(mean * torch.ones(1, dtype=torch.float32, device=self.dummy_param.device))
         self.std = nn.Parameter(std * torch.ones(1, dtype=torch.float32, device=self.dummy_param.device))
+        self.mean_np = self.mean.cpu().data.numpy()
+        self.std_np = self.std.cpu().data.numpy()
 
     def normalise(self, values):
-        return (values - self.mean) / (self.std + 1e-4)
+        return (values - self.mean_np) / (self.std_np + 1e-4)
 
     def denormalise(self, normalised_values):
-        return self.mean + normalised_values * self.std
+        return self.mean_np + normalised_values * self.std_np
